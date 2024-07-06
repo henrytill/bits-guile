6a3e0bfac7bf2e08e456f6fc491d523fe03c15bf
Henry Till <henrytill@gmail.com>
2024-07-05 21:23:38 -0700

    add initial work

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    let
+      makeGuileBits =
+        pkgs:
+        pkgs.stdenv.mkDerivation {
+          name = "guile-bits";
+          src = builtins.path {
+            path = ./.;
+            name = "guile-bits-src";
+          };
+          nativeBuildInputs = with pkgs; [
+            autoreconfHook
+            pkg-config
+          ];
+          buildInputs = with pkgs; [ guile ];
+          installFlags = [ "DESTDIR=${placeholder "out"}" ];
+        };
+    in
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages.guile-bits = makeGuileBits pkgs;
+        packages.default = self.packages.${system}.guile-bits;
+      }
+    );
+}
---
+++ main.c
@@ -0,0 +1,10 @@
+#include <libguile.h>
+
+static void inner_main(__attribute__((unused)) void *closure, int argc, char *argv[]) {
+  scm_shell(argc, argv);
+}
+
+int main(int argc, char *argv[]) {
+  scm_boot_guile(argc, argv, inner_main, 0);
+  return 0;
+}
