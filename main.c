b9c1a48b2fafa1817b515c4f86bbca4d4d5b13cd
Henry Till <henrytill@gmail.com>
2024-11-02 03:15:45 -0700

    update .clang-format and reformat

--- main.c
+++ main.c
@@ -2,21 +2,26 @@
 
 #include "config.h"
 
-static SCM guile_bits_version(void) {
-  const char *s = PACKAGE_VERSION;
-  if (s == NULL) {
-    return SCM_BOOL_F;
-  } else {
-    return scm_from_locale_string(s);
-  }
+#define NAME_GUILE_BITS_VERSION "guile-bits-version"
+
+static SCM guile_bits_version(void)
+{
+    const char *s = PACKAGE_VERSION;
+    if (s == NULL) {
+        return SCM_BOOL_F;
+    } else {
+        return scm_from_locale_string(s);
+    }
 }
 
-static void inner_main(__attribute__((unused)) void *closure, int argc, char *argv[]) {
-  scm_c_define_gsubr("guile-bits-version", 0, 0, 0, guile_bits_version);
-  scm_shell(argc, argv);
+static void inner_main(__attribute__((unused)) void *closure, int argc, char *argv[])
+{
+    scm_c_define_gsubr(NAME_GUILE_BITS_VERSION, 0, 0, 0, guile_bits_version);
+    scm_shell(argc, argv);
 }
 
-int main(int argc, char *argv[]) {
-  scm_boot_guile(argc, argv, inner_main, 0);
-  return 0;
+int main(int argc, char *argv[])
+{
+    scm_boot_guile(argc, argv, inner_main, 0);
+    return 0;
 }
